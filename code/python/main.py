d155171a44639db1c365acebed0063ae99ce9df4
LarsZondag <larszondag@gmail.com>
2016-03-10 15:54:00 +0100

    Making random steps now

--- code/python/main.py
+++ code/python/main.py
@@ -1,8 +1,9 @@
 import numpy as np
 from numba import jit
+import matplotlib.pyplot as plt
 
 N = 100
-L=0
+L=2
 
 eps = 0.25
 sigma = 0.8
@@ -10,24 +11,41 @@ T = 1
 
 polymer = np.zeros((N, 2))
 N_angles = 6
-d_theta = np.pi / (N_angles + 1)
-angles = np.linspace(d_theta, N_angles * d_theta, N_angles)
+d_theta = 2 * np.pi / (N_angles + 1)
+angles = np.linspace(d_theta - np.pi, N_angles * d_theta - np.pi, N_angles)
+pol_weight = 1
 
 # Initialize:
-polymer[1,0] = 1
+polymer[1,1] = 1
 
-def calc_boltz_weights(L):
-    dr = polymer[L-1] - polymer[L-2]
-    if dr[2] == 0:
-        angle_offset = 0
-    else:
-        angle_offset = np.arctan(dr[1]/dr[2])
-    possible_positions = polymer[L-1, 0] + np.cos(angles + angle_offset)
+def calc_boltz_weights(bead):
+    dr = polymer[bead-1] - polymer[bead-2]
+    angle_offset = np.arctan2(dr[1],dr[0])
+    print("Angle offset: " + repr(angle_offset * 180 / np.pi))
+    possible_positions = np.zeros((N_angles, 2))
+    possible_positions[:,0] = polymer[bead-1, 0] + np.cos(angles + angle_offset)
+    possible_positions[:,1] = polymer[bead-1, 1] + np.sin(angles + angle_offset)
+    energies = calc_energy(possible_positions)
+    # weights = np.exp(-energies/T)
+    # return weights, np.sum(weights)
+    return possible_positions
 
 
 def calc_energy(possible_positions):
     pass
 
-def add_bead(polymer, pol_weight, L):
 
-    pass
+def add_bead(polymer, pol_weight, L):
+    possible_positions = calc_boltz_weights(L)
+    new_position = possible_positions[int (np.random.uniform() * N_angles)]
+    polymer[L] = new_position
+    if L < N - 1:
+        add_bead(polymer, pol_weight, L+1)
+    else:
+        return polymer
+
+
+add_bead(polymer, pol_weight, L)
+
+plt.plot(polymer[:,0], polymer[:,1])
+plt.show()
