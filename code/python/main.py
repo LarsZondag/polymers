3f5bec4d306f57553612fdbabeb135be802a9257
LarsZondag <larszondag@gmail.com>
2016-03-10 14:54:40 +0100

    Made a start. Sadiq will work on energy calculation. Lars works on possible positions

--- code/python/main.py
+++ code/python/main.py
@@ -8,13 +8,24 @@ eps = 0.25
 sigma = 0.8
 T = 1
 
-locations = np.array((N, 2))
+polymer = np.zeros((N, 2))
 N_angles = 6
 d_theta = np.pi / (N_angles + 1)
 angles = np.linspace(d_theta, N_angles * d_theta, N_angles)
 
-def calc_boltz_weights():
+# Initialize:
+polymer[1,0] = 1
 
+def calc_boltz_weights(L):
+    dr = polymer[L-1] - polymer[L-2]
+    if dr[2] == 0:
+        angle_offset = 0
+    else:
+        angle_offset = np.arctan(dr[1]/dr[2])
+    possible_positions = polymer[L-1, 0] + np.cos(angles + angle_offset)
+
+
+def calc_energy(possible_positions):
     pass
 
 def add_bead(polymer, pol_weight, L):
