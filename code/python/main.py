b5d62312d547a3233d45cd7c865f17b4b8168aea
SvanO <sadiqvanoverbeek@gmail.com>
2016-03-17 15:41:31 +0100

    Code for Jos Thijssen

--- code/python/main.py
+++ code/python/main.py
@@ -3,7 +3,7 @@ from numba import jit
 import matplotlib.pyplot as plt
 import bisect
 
-N = 4
+N = 100
 L=2
 
 eps = 0.25
@@ -12,10 +12,9 @@ sigma2 = sigma**2
 T = 1
 
 polymer = np.zeros((N, 2))
-N_angles = 4
-d_theta = 2 * np.pi / (N_angles)
-angles = np.linspace(0, 2*np.pi-d_theta, N_angles)
-dr = np.array(([1, 0, -1, 0],[0, 1, 0, -1]))
+N_angles = 6
+d_theta = 2 * np.pi / (N_angles + 1)
+# angles = np.linspace(d_theta - np.pi, N_angles * d_theta - np.pi, N_angles)
 pol_weight = 1
 random_numbers = np.random.uniform(0, 1, N)
 
@@ -25,36 +24,32 @@ polymer[1,1] = 1
 
 def calc_boltz_weights(bead):
     possible_positions = np.zeros((N_angles, 2))
-    possible_positions[:,0] = polymer[bead-1, 0] + dr[0,:]
-    possible_positions[:,1] = polymer[bead-1, 1] + dr[1,:]
-    plausible_positions, energies = calc_energy(possible_positions, bead)
+    angles = np.linspace(0, 2* np.pi, N_angles) + 2 * np.pi * np.random.rand()
+    possible_positions[:,0] = polymer[bead-1, 0] + np.cos(angles)
+    possible_positions[:,1] = polymer[bead-1, 1] + np.sin(angles)
+    energies = calc_energy(possible_positions, bead)
     weights = np.exp(-energies/T)
     W = np.sum(weights)
     weights = np.cumsum(weights)/W
-    return plausible_positions, weights, W
+    return possible_positions, weights, W
 
 
+@jit
 def calc_energy(possible_positions, bead):
-    plausible_positions = np.copy(possible_positions)
-    for i in range(N_angles):
-        if plausible_positions[i,0] in polymer[:bead,0]:
-            if plausible_positions[i,1] in polymer[:bead,1]:
-                plausible_positions = np.delete(plausible_positions, i, 0)
-
-    size = plausible_positions[:,0].size
+    size = possible_positions[:,0].size
     interaction_energy = np.zeros(size)
     interaction_energy += 4*eps*(sigma**12-sigma**6)
-    for i in range(bead-1):
-        for j in range(size):
-            possible_positions_dx = plausible_positions[j, 0] - polymer[i,0]
-            possible_positions_dy = plausible_positions[j, 1] - polymer[i,1]
-            distance = possible_positions_dx*possible_positions_dx + possible_positions_dy*possible_positions_dy
-            ir2 = sigma2 / distance
-            ir6 = ir2 * ir2 * ir2
-            ir12 = ir6 * ir6
-            interaction_energy[j] += 4 * eps * (ir12 - ir6)
-    print(plausible_positions)
-    return plausible_positions, interaction_energy
+    for j in range(size):
+        for i in range(bead-1):
+                possible_positions_dx = possible_positions[j, 0] - polymer[i,0]
+                possible_positions_dy = possible_positions[j, 1] - polymer[i,1]
+                distance = possible_positions_dx*possible_positions_dx + possible_positions_dy*possible_positions_dy
+                ir2 = sigma2 / distance
+                ir6 = ir2 * ir2 * ir2
+                ir12 = ir6 * ir6
+                interaction_energy[j] += 4 * eps * (ir12 - ir6)
+    return interaction_energy
+
 
 for i in range(L, N):
     possible_positions, w_i, W = calc_boltz_weights(i)
