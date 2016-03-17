90a8f638595f5c619a384a1477386dc108524f28
LarsZondag <larszondag@gmail.com>
2016-03-17 14:02:24 +0100

    Now deleting the right entry

--- code/python/main.py
+++ code/python/main.py
@@ -3,7 +3,7 @@ from numba import jit
 import matplotlib.pyplot as plt
 import bisect
 
-N = 50
+N = 4
 L=2
 
 eps = 0.25
@@ -15,7 +15,7 @@ polymer = np.zeros((N, 2))
 N_angles = 4
 d_theta = 2 * np.pi / (N_angles)
 angles = np.linspace(0, 2*np.pi-d_theta, N_angles)
-dr = np.array((np.cos(angles), np.sin(angles)))
+dr = np.array(([1, 0, -1, 0],[0, 1, 0, -1]))
 pol_weight = 1
 random_numbers = np.random.uniform(0, 1, N)
 
@@ -36,18 +36,19 @@ def calc_boltz_weights(bead):
 
 def calc_energy(possible_positions, bead):
     plausible_positions = np.copy(possible_positions)
-    interaction_energy = np.zeros(N_angles)
+    for i in range(N_angles):
+        if plausible_positions[i,0] in polymer[:bead,0]:
+            if plausible_positions[i,1] in polymer[:bead,1]:
+                plausible_positions = np.delete(plausible_positions, i, 0)
+
+    size = plausible_positions[:,0].size
+    interaction_energy = np.zeros(size)
     interaction_energy += 4*eps*(sigma**12-sigma**6)
     for i in range(bead-1):
-        for j in range(N_angles):
+        for j in range(size):
             possible_positions_dx = plausible_positions[j, 0] - polymer[i,0]
             possible_positions_dy = plausible_positions[j, 1] - polymer[i,1]
             distance = possible_positions_dx*possible_positions_dx + possible_positions_dy*possible_positions_dy
-            if distance < 10:
-                np.delete(plausible_positions, j, 0)
-                np.delete(interaction_energy, j, 0)
-                print("DELETED")
-                break
             ir2 = sigma2 / distance
             ir6 = ir2 * ir2 * ir2
             ir12 = ir6 * ir6
