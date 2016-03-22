b42938c6ef175cc4ae0366fe273f25d8ec7f0176
LarsZondag <larszondag@gmail.com>
2016-03-22 09:12:05 +0100

    BACK TO 2D

--- code/python/Polymer.py
+++ code/python/Polymer.py
@@ -8,10 +8,8 @@ class Polymer:
     def __init__(self, length, number_of_angles, temperature):
         self.L = length
         self.N_angles = number_of_angles
-        self.N_angles_total = self.N_angles ** 2
-        self.population = np.zeros((self.L, 3))
-        self.azimuthal_angles = np.linspace(0, 2*np.pi, self.N_angles, False)
-        self.polar_angles = np.linspace(0, np.pi, self.N_angles)
+        self.population = np.zeros((self.L, 2))
+        self.angles = np.linspace(0, 2*np.pi, self.N_angles, False)
         self.bead = 1
         self.sigma = 0.8
         self.sigma2 = self.sigma ** 2
@@ -20,37 +18,36 @@ class Polymer:
 
 
     def populate(self):
-        self.random_azimuthal_angles = 2 * np.pi * np.random.rand(self.L)
-        self.random_polar_angles = np.pi * np.random.rand(self.L)
+        self.pol_weight = 1
+        self.random_angles = 2 * np.pi * np.random.rand(self.L)
+        print(self.random_angles)
         while self.bead < self.L:
-            new_pos = np.zeros((self.N_angles_total, 3))
-            polar_angles, azimuthal_angles = self.calc_angles()
-            for i in range(self.N_angles):
-                for j in range(self.N_angles):
-                    new_pos[i*self.N_angles + j,0] = self.population[self.bead - 1, 0] + np.sin(polar_angles[i]) * np.cos(azimuthal_angles[j])
-                    new_pos[i*self.N_angles + j,1] = self.population[self.bead - 1, 1] + np.sin(polar_angles[i]) * np.sin(azimuthal_angles[j])
-                    new_pos[i*self.N_angles + j,2] = self.population[self.bead - 1, 2] + np.cos(polar_angles[i])
+            new_pos = np.zeros((self.N_angles, 2))
+            angles = self.calc_angles()
+            new_pos[:,0] = self.population[self.bead - 1, 0] + np.cos(angles)
+            new_pos[:,1] = self.population[self.bead - 1, 1] + np.sin(angles)
             energy = self.calc_energy(new_pos)
             weights = self.calc_weights(energy)
             if not np.any(weights):
                 print("I GOT STUCK! I left at bead: ",self.bead, "I will try again.")
                 self.bead = 1
-                self.population = np.zeros((self.L, 3))
+                self.population = np.zeros((self.L, 2))
                 self.populate()
                 break
             index = self.choose_angle(weights)
             self.population[self.bead] = new_pos[index]
+            # self.pol_weight *= np.sum(weights)
             self.bead += 1
+        # print("polweight: ", self.pol_weight)
         return self.population
 
     def calc_energy(self, possible_positions):
-        interaction_energy = np.zeros(self.N_angles_total)
+        interaction_energy = np.zeros(self.N_angles)
         for j in range(self.N_angles):
             for i in range(self.bead):
                 dx = possible_positions[j, 0] - self.population[i,0]
                 dy = possible_positions[j, 1] - self.population[i,1]
-                dz = possible_positions[j, 2] - self.population[i,2]
-                d2 = dx*dx + dy*dy + dz * dz
+                d2 = dx*dx + dy*dy
                 ir2 = self.sigma2 / d2
                 ir6 = ir2 * ir2 * ir2
                 ir12 = ir6 * ir6
@@ -58,11 +55,9 @@ class Polymer:
         return interaction_energy
 
     def calc_angles(self):
-        random_azimuthal_angle = self.random_azimuthal_angles[self.bead]
-        random_polar_angle = self.random_polar_angles[self.bead]
-        azimuthal_angle = self.azimuthal_angles + random_azimuthal_angle
-        polar_angle = (self.polar_angles + random_polar_angle) % np.pi
-        return polar_angle, azimuthal_angle
+        random_angle = self.random_angles[self.bead]
+        angles = self.angles + random_angle
+        return angles
 
     def calc_weights(self, energy):
         weights = np.exp(-energy/self.T)
@@ -75,5 +70,8 @@ class Polymer:
         while random >= cumsum[pos_index] and pos_index <= self.N_angles:
             pos_index += 1
         return pos_index
+
+    def get_end_to_end(self):
+        return np.linalg.norm(self.population[0] - self.population[-1]) ** 2
 
 
