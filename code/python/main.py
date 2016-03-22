83a7f28c5659409e4fdfee0e03312409d0317dbb
LarsZondag <larszondag@gmail.com>
2016-03-22 09:12:32 +0100

    BACK TO 2D

--- code/python/main.py
+++ code/python/main.py
@@ -10,38 +10,39 @@ length_steps = 5
 lengths = np.linspace(starting_length, stop_length, length_steps)
 lengths = np.ceil(lengths)
 T = 1
-N_angles = 2
+N_angles = 6
 pop_size = 100000
+N = 50
 
 
-# pol = Polymer(N, N_angles, T)
-# pol.populate()
-# print(pol.get_end_to_end())
-#
-# fig = plt.figure()
+pol = Polymer(N, N_angles, T)
+pol.populate()
+print(pol.get_end_to_end())
+
+fig = plt.figure()
 # ax = fig.add_subplot(111, projection='3d')
-# ax.plot(pol.population[:,0], pol.population[:,1], pol.population[:,2])
-# plt.show()
-
-end_to_end = np.zeros((length_steps, pop_size))
-for i in range(length_steps):
-    polymer = Polymer(lengths[i], N_angles, T)
-    for j in range(pop_size):
-        polymer.populate()
-        end_to_end[i, j] = polymer.get_end_to_end()
-
-mean_ETE = np.mean(end_to_end, axis= 1)
-
-def func (N, nu):
-    return np.power(N, nu)
-
-popt, pcov = curve_fit(func, lengths, mean_ETE)
-print("popt: ", popt)
-print("pcov: ", pcov)
-
-
-
-plt.loglog(lengths, mean_ETE, lengths, np.power(lengths, popt))
+plt.plot(pol.population[:,0], pol.population[:,1])
 plt.show()
+
+# end_to_end = np.zeros((length_steps, pop_size))
+# for i in range(length_steps):
+#     polymer = Polymer(lengths[i], N_angles, T)
+#     for j in range(pop_size):
+#         polymer.populate()
+#         end_to_end[i, j] = polymer.get_end_to_end()
+#
+# mean_ETE = np.mean(end_to_end, axis= 1)
+#
+# def func (N, nu):
+#     return np.power(N, nu)
+#
+# popt, pcov = curve_fit(func, lengths, mean_ETE)
+# print("popt: ", popt)
+# print("pcov: ", pcov)
+
+
+
+# plt.loglog(lengths, mean_ETE, lengths, np.power(lengths, popt))
+# plt.show()
 
 
